9573ef743aa3c1627dbbb95e0c2de7e8d67260d5
nicey0 <nicey_pm@protonmail.com>
2020-11-05 17:36:09 -0300

    no

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 mod pieces;
 
-use pieces::{o, i, j, l, s, z, t};
+use pieces::{Piece, o, i, j, l, s, z, t};
+use std::{thread, time};
 
 const MAXX: i8 = 10;
 const MAXY: i8 = 22;
 
 fn main() {
-    let mut piece = o();
+    let mut pieces: Vec<Piece> = Vec::new();
+    let mut piece = o(MAXX, MAXY);
+    loop {
+        print!("\x1B[2J\x1B[1;1H");
+        if !piece.step(1) {
+            // TODO: switch to new piece
+            pieces.push(piece.clone());
+            piece = i(MAXX, MAXY);
+        }
+        for y in 0..MAXY {
+            for x in 0..MAXX {
+                if piece.get_shape().contains(&(x, y)) {
+                    print!("X ");
+                } else {
+                    print!(". ");
+                }
+            }
+            println!("");
+        }
+        thread::sleep(time::Duration::from_millis(500));
+    }
 }
--- src/pieces.rs
+++ src/pieces.rs
@@ -7,17 +7,22 @@ pub struct Piece {
     color: [f32; 4],
     rots: Rotations,
     idx: usize,
+    maxx: i8,
+    maxy: i8,
 }
 
 impl Piece {
-    pub fn new(color: [f32; 4], rots: Rotations) -> Self {
-        Piece { color, rots, idx: 0 }
+    pub fn new(color: [f32; 4], rots: Rotations, maxx: i8, maxy: i8) -> Self {
+        Piece { color, rots, idx: 0, maxx, maxy }
     }
 
     pub fn shift(&mut self, c: (i8, i8)) {
         for rot in self.rots.iter_mut() {
             for mut coor in rot {
                 coor.0 += c.0;
+                if coor.0 >= self.maxx {
+                    coor.0 += self.maxx - coor.0 - 2;
+                }
                 coor.1 += c.1;
             }
         }
@@ -33,91 +38,80 @@ impl Piece {
         }
     }
 
-    pub fn validate(&self, maxx: i8, maxy: i8) -> (i8, i8) {
-        let (mut tx, mut ty);
-        let (mut x, mut y) = (0, 0);
-        for coor in self.get_shape() {
-            // test x validity
-            if coor.0 >= maxx {
-                tx = maxx - coor.0 - 1;
-                if tx < x { x = tx; }
-            } else if coor.0 < 0 {
-                tx = -coor.0;
-                if tx > x { x = tx; }
-            }
-            // test y validity
-            if coor.1 >= maxy {
-                ty = maxy - coor.1 - 1;
-                if ty < y { y = ty; }
-            } else if coor.1 < 0 {
-                ty = -coor.1;
-                if ty > y { y = ty; }
-            }
-        }
-        (x, y)
-    }
-
-    pub fn fix(&mut self, maxx: i8, maxy: i8) {
-        self.shift(self.validate(maxx, maxy));
-    }
-
     pub fn get_shape(&self) -> &Vec<(i8, i8)> {
         &self.rots[self.idx]
     }
+
+    pub fn step(&mut self, x: i8) -> bool {
+        for (_, y) in self.get_shape().iter() {
+            if y == &(self.maxy-1) {
+                return false
+            }
+        }
+        self.shift((x, 1));
+        true
+    }
 }
 
-pub fn o() -> Piece {
+pub fn o(maxx: i8, maxy: i8) -> Piece {
     Piece::new([1.0, 1.0, 0.0, 1.0], [
     vec![(0, 0), (0, 1), (1, 0), (1, 1)],
     vec![(0, 0), (0, 1), (1, 0), (1, 1)],
     vec![(0, 0), (0, 1), (1, 0), (1, 1)],
-    vec![(0, 0), (0, 1), (1, 0), (1, 1)],])
+    vec![(0, 0), (0, 1), (1, 0), (1, 1)],],
+    maxx, maxy)
 }
 
-pub fn i() -> Piece {
+pub fn i(maxx: i8, maxy: i8) -> Piece {
     Piece::new([0.0, 1.0, 1.0, 1.0], [
     vec![(0, 1), (1, 1), (2, 1), (3, 1)],
     vec![(2, 0), (2, 1), (2, 2), (2, 3)],
     vec![(0, 2), (1, 2), (2, 2), (3, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (1, 3)],])
+    vec![(1, 0), (1, 1), (1, 2), (1, 3)],],
+    maxx, maxy)
 }
 
-pub fn j() -> Piece {
+pub fn j(maxx: i8, maxy: i8) -> Piece {
     Piece::new([0.0, 0.0, 1.0, 1.0], [
     vec![(0, 1), (1, 1), (2, 1), (0, 0)],
     vec![(1, 0), (1, 1), (1, 2), (2, 0)],
     vec![(0, 1), (1, 1), (2, 1), (2, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (0, 2)],])
+    vec![(1, 0), (1, 1), (1, 2), (0, 2)],],
+    maxx, maxy)
 }
 
-pub fn l() -> Piece {
+pub fn l(maxx: i8, maxy: i8) -> Piece {
     Piece::new([1.0, 0.5, 0.0, 1.0], [
     vec![(0, 1), (1, 1), (2, 1), (2, 0)],
     vec![(1, 0), (1, 1), (1, 2), (2, 2)],
     vec![(0, 1), (1, 1), (2, 1), (0, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (0, 0)],])
+    vec![(1, 0), (1, 1), (1, 2), (0, 0)],],
+    maxx, maxy)
 }
 
-pub fn s() -> Piece {
+pub fn s(maxx: i8, maxy: i8) -> Piece {
     Piece::new([0.0, 1.0, 0.0, 1.0], [
     vec![(0, 1), (1, 1), (1, 0), (2, 0)],
     vec![(1, 0), (1, 1), (2, 1), (2, 2)],
     vec![(0, 2), (1, 2), (1, 1), (2, 1)],
-    vec![(0, 0), (0, 1), (1, 1), (1, 2)],])
+    vec![(0, 0), (0, 1), (1, 1), (1, 2)],],
+    maxx, maxy)
 }
 
-pub fn z() -> Piece {
+pub fn z(maxx: i8, maxy: i8) -> Piece {
     Piece::new([1.0, 0.0, 0.0, 1.0], [
     vec![(0, 0), (1, 0), (1, 1), (2, 1)],
     vec![(1, 1), (1, 2), (2, 1), (2, 0)],
     vec![(0, 1), (1, 1), (1, 2), (2, 2)],
-    vec![(1, 0), (1, 1), (0, 1), (0, 2)],])
+    vec![(1, 0), (1, 1), (0, 1), (0, 2)],],
+    maxx, maxy)
 }
 
-pub fn t() -> Piece {
+pub fn t(maxx: i8, maxy: i8) -> Piece {
     Piece::new([0.5, 0.0, 0.8, 1.0], [
     vec![(0, 1), (1, 1), (2, 1), (1, 0)],
     vec![(1, 0), (1, 1), (1, 2), (2, 1)],
     vec![(0, 1), (1, 1), (2, 1), (1, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (0, 1)],])
+    vec![(1, 0), (1, 1), (1, 2), (0, 1)],],
+    maxx, maxy)
 }
