4d4394061cacbf31b13b28594f36a66c1fa90cb9
nicey0 <nicey_pm@protonmail.com>
2020-11-06 11:41:42 -0300

    comments

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,25 @@ fn random_piece() -> Piece {
 }
 
 fn main() {
+    // Vector holding still pieces
     let mut pieces: Vec<Point> = Vec::new();
+    // Vector holding still pieces' colors (I know it's weird, deal with it)
     let mut colors: Vec<Color> = Vec::new();
     let mut p = random_piece();
     loop {
-        print!("\x1B[2J\x1B[1;1H");
+        print!("\x1B[2J\x1B[1;1H"); // clear scr
         print_board(&p, &pieces);
-        match p.down(1, &pieces) {
-            States::Stop => { for &s in p.get_shape().iter() { pieces.push(s); colors.push(*p.get_color()) } p = random_piece(); },
+        match p.down(1, &pieces) { // gravity
+            States::Stop => { // if illegal position, stop moving and instantiate new piece
+                for &s in p.get_shape().iter() {
+                    pieces.push(s); colors.push(*p.get_color())
+                }
+                p = random_piece();
+            }, // if outside screen & illegal position, end game
             States::End => break,
             _ => {},
         };
-        p.side(1, &pieces);
+        p.side(-1, &pieces);
         p.rotate(&pieces);
         thread::sleep(time::Duration::from_millis(120));
     }
--- src/pieces.rs
+++ src/pieces.rs
@@ -33,42 +33,47 @@ impl Piece {
     }
 
     pub fn side(&mut self, c: i8, pieces: &Vec<Point>) {
-        let mut shape: [Point; 4] = [(0, 0); 4];
-        let mut origin: FPoint = self.origin;
+        // move in x axis
+        let mut shape: [Point; 4] = [(0, 0); 4]; // shape after movement
+        let mut origin: FPoint = self.origin; // origin after movement
         for i in 0..self.shape.len() {
             shape[i] = (self.shape[i].0 + c, self.shape[i].1);
             origin = (self.origin.0 + c as f64, self.origin.1);
             if shape[i].0 < 0 || shape[i].0 >= self.maxx 
                     || pieces.contains(&shape[i]) {
-                return
+                return // return if illegal position
             }
         }
+        // set shape and origin to moved values
         self.shape = shape;
         self.origin = origin;
     }
 
     pub fn down(&mut self, c: i8, pieces: &Vec<Point>) -> States {
-        let mut shape: [Point; 4] = [(0, 0); 4];
-        let mut origin: FPoint = self.origin;
+        // move in y axis
+        let mut shape: [Point; 4] = [(0, 0); 4]; // shape after movement
+        let mut origin: FPoint = self.origin; // origin after movement
         for i in 0..self.shape.len() {
             shape[i] = (self.shape[i].0, self.shape[i].1 + c);
             origin = (self.origin.0, self.origin.1 + c as f64);
             if shape[i].1 >= self.maxy  || pieces.contains(&shape[i]) {
                 return if shape[i].1 <= self.maxy - self.boardy - 2 {
-                    States::End
+                    States::End // return End if illegal position & outside screen
                 } else {
-                    States::Stop
+                    States::Stop // else return Stop
                 }
             }
         }
+        // set shape and origin to moved values & return Nothing
         self.shape = shape;
         self.origin = origin;
         States::Nothing
     }
 
     pub fn rotate(&mut self, pieces: &Vec<Point>) {
-        let mut shape: [Point; 4] = [(0, 0); 4];
+        let mut shape: [Point; 4] = [(0, 0); 4]; // shape after rotation
         for i in 0..self.shape.len() {
+            // math
             let coor = self.shape[i];
             let adj_x: f64 = (coor.0 as f64 - self.origin.0) * -1.0;
             let adj_y: f64 = (coor.1 as f64 - self.origin.1) * -1.0;
@@ -76,11 +81,13 @@ impl Piece {
             let rot_y: f64 = adj_x * (PI/2.0).sin() + adj_y * (PI/2.0).cos();
             shape[i].0 = (rot_x * -1.0 + self.origin.0).round() as i8;
             shape[i].1 = (rot_y * -1.0 + self.origin.1).round() as i8;
+            // return if illegal position
             if shape[i].0 < 0 || shape[i].0 >= self.maxx || shape[i].1 < 0 || shape[i].1 >= self.maxy ||
                     pieces.contains(&shape[i]) {
                 return
             }
         }
+        // set shape to moved value
         self.shape = shape;
     }
 
