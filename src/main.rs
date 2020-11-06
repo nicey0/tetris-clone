9bc0781cbc33964a43dedcf83c44466fd85b58bc
nicey0 <nicey_pm@protonmail.com>
2020-11-06 11:04:24 -0300

    i forgot git was a thing haha oopsie

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
+extern crate rand;
+
 mod pieces;
 
-use pieces::{Piece, o, i, j, l, s, z, t};
+use pieces::*;
 use std::{thread, time};
+use rand::seq::SliceRandom;
 
 const MAXX: i8 = 10;
-const MAXY: i8 = 22;
+const MAXY: i8 = 26;
+const BOARDY: i8 = 22;
+const PIECES: [fn(i8, i8, i8) -> Piece; 7] = [i, o, j, l, s, z, t];
+
+fn random_piece() -> Piece {
+    PIECES.choose(&mut rand::thread_rng()).unwrap()(MAXX, MAXY, BOARDY)
+}
 
 fn main() {
-    let mut pieces: Vec<Piece> = Vec::new();
-    let mut piece = o(MAXX, MAXY);
+    let mut pieces: Vec<Point> = Vec::new();
+    let mut colors: Vec<Color> = Vec::new();
+    let mut p = random_piece();
     loop {
         print!("\x1B[2J\x1B[1;1H");
-        if !piece.step(1) {
-            // TODO: switch to new piece
-            pieces.push(piece.clone());
-            piece = i(MAXX, MAXY);
-        }
-        for y in 0..MAXY {
-            for x in 0..MAXX {
-                if piece.get_shape().contains(&(x, y)) {
-                    print!("X ");
-                } else {
-                    print!(". ");
-                }
+        print_board(&p, &pieces);
+        match p.down(1, &pieces) {
+            States::Stop => { for &s in p.get_shape().iter() { pieces.push(s); colors.push(*p.get_color()) } p = random_piece(); },
+            States::End => break,
+            _ => {},
+        };
+        p.side(1, &pieces);
+        p.rotate(&pieces);
+        thread::sleep(time::Duration::from_millis(120));
+    }
+}
+
+fn print_board(p: &Piece, pieces: &Vec<Point>) {
+    let s =  p.get_shape();
+    println!("{:?}", s);
+    for y in MAXY-BOARDY..MAXY {
+        for x in 0..MAXX {
+            if s.contains(&(x, y)) {
+                print!("██");
+            } else if pieces.contains(&(x, y)) {
+                print!("🮕🮕");
+            } else {
+                print!(" .");
             }
-            println!("");
         }
-        thread::sleep(time::Duration::from_millis(500));
+        println!("");
     }
 }
--- src/pieces.rs
+++ src/pieces.rs
@@ -1,117 +1,145 @@
-const ROT: usize = 4;
+use std::f64::consts::PI;
 
-pub type Rotations = [Vec<(i8, i8)>; ROT];
+pub type Color = [f32; 4];
+pub type Point = (i8, i8);
+pub type FPoint = (f64, f64);
+
+pub enum States {
+    Stop,
+    End,
+    Nothing
+}
 
 #[derive(Clone, Debug)]
 pub struct Piece {
-    color: [f32; 4],
-    rots: Rotations,
-    idx: usize,
+    color: Color,
+    shape: [Point; 4],
+    origin: FPoint,
     maxx: i8,
     maxy: i8,
+    boardy: i8,
 }
 
 impl Piece {
-    pub fn new(color: [f32; 4], rots: Rotations, maxx: i8, maxy: i8) -> Self {
-        Piece { color, rots, idx: 0, maxx, maxy }
+    pub fn new(color: Color, shape: [Point; 4], origin: FPoint, maxx: i8, maxy: i8, boardy: i8) -> Self {
+        Self {
+            color,
+            shape,
+            origin,
+            maxx,
+            maxy,
+            boardy,
+        }
     }
 
-    pub fn shift(&mut self, c: (i8, i8)) {
-        for rot in self.rots.iter_mut() {
-            for mut coor in rot {
-                coor.0 += c.0;
-                if coor.0 >= self.maxx {
-                    coor.0 += self.maxx - coor.0 - 2;
+    pub fn side(&mut self, c: i8, pieces: &Vec<Point>) {
+        let mut shape: [Point; 4] = [(0, 0); 4];
+        let mut origin: FPoint = self.origin;
+        for i in 0..self.shape.len() {
+            shape[i] = (self.shape[i].0 + c, self.shape[i].1);
+            origin = (self.origin.0 + c as f64, self.origin.1);
+            if shape[i].0 < 0 || shape[i].0 >= self.maxx 
+                    || pieces.contains(&shape[i]) {
+                return
+            }
+        }
+        self.shape = shape;
+        self.origin = origin;
+    }
+
+    pub fn down(&mut self, c: i8, pieces: &Vec<Point>) -> States {
+        let mut shape: [Point; 4] = [(0, 0); 4];
+        let mut origin: FPoint = self.origin;
+        for i in 0..self.shape.len() {
+            shape[i] = (self.shape[i].0, self.shape[i].1 + c);
+            origin = (self.origin.0, self.origin.1 + c as f64);
+            if shape[i].1 >= self.maxy  || pieces.contains(&shape[i]) {
+                return if shape[i].1 <= self.maxy - self.boardy - 2 {
+                    States::End
+                } else {
+                    States::Stop
                 }
-                coor.1 += c.1;
             }
         }
+        self.shape = shape;
+        self.origin = origin;
+        States::Nothing
     }
 
-    pub fn rotate(&mut self, dir: bool) {
-        self.idx = if dir { // clockwise
-            if self.idx < ROT-1 { self.idx + 1 }
-            else { 0 }
-        } else { // counter-clockwise
-            if self.idx > 0 { self.idx - 1 }
-            else { ROT-1 }
-        }
-    }
-
-    pub fn get_shape(&self) -> &Vec<(i8, i8)> {
-        &self.rots[self.idx]
-    }
-
-    pub fn step(&mut self, x: i8) -> bool {
-        for (_, y) in self.get_shape().iter() {
-            if y == &(self.maxy-1) {
-                return false
+    pub fn rotate(&mut self, pieces: &Vec<Point>) {
+        let mut shape: [Point; 4] = [(0, 0); 4];
+        for i in 0..self.shape.len() {
+            let coor = self.shape[i];
+            let adj_x: f64 = (coor.0 as f64 - self.origin.0) * -1.0;
+            let adj_y: f64 = (coor.1 as f64 - self.origin.1) * -1.0;
+            let rot_x: f64 = adj_x * (PI/2.0).cos() - adj_y * (PI/2.0).sin();
+            let rot_y: f64 = adj_x * (PI/2.0).sin() + adj_y * (PI/2.0).cos();
+            shape[i].0 = (rot_x * -1.0 + self.origin.0).round() as i8;
+            shape[i].1 = (rot_y * -1.0 + self.origin.1).round() as i8;
+            if shape[i].0 < 0 || shape[i].0 >= self.maxx || shape[i].1 < 0 || shape[i].1 >= self.maxy ||
+                    pieces.contains(&shape[i]) {
+                return
             }
         }
-        self.shift((x, 1));
-        true
+        self.shape = shape;
+    }
+
+    pub fn get_shape(&self) -> &[Point; 4] {
+        &self.shape
+    }
+
+    pub fn get_color(&self) -> &Color {
+        &self.color
     }
 }
 
-pub fn o(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([1.0, 1.0, 0.0, 1.0], [
-    vec![(0, 0), (0, 1), (1, 0), (1, 1)],
-    vec![(0, 0), (0, 1), (1, 0), (1, 1)],
-    vec![(0, 0), (0, 1), (1, 0), (1, 1)],
-    vec![(0, 0), (0, 1), (1, 0), (1, 1)],],
-    maxx, maxy)
+
+pub fn i(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([1.0, 1.0, 0.0, 1.0],
+               [(0, 0), (1, 0), (2, 0), (3, 0)],
+               (1.5, 0.0),
+               maxx, maxy, boardy)
 }
 
-pub fn i(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([0.0, 1.0, 1.0, 1.0], [
-    vec![(0, 1), (1, 1), (2, 1), (3, 1)],
-    vec![(2, 0), (2, 1), (2, 2), (2, 3)],
-    vec![(0, 2), (1, 2), (2, 2), (3, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (1, 3)],],
-    maxx, maxy)
+pub fn o(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([0.0, 1.0, 1.0, 1.0],
+               [(0, 0), (1, 0), (0, 1), (1, 1)],
+               (0.5, 0.5),
+               maxx, maxy, boardy)
 }
 
-pub fn j(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([0.0, 0.0, 1.0, 1.0], [
-    vec![(0, 1), (1, 1), (2, 1), (0, 0)],
-    vec![(1, 0), (1, 1), (1, 2), (2, 0)],
-    vec![(0, 1), (1, 1), (2, 1), (2, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (0, 2)],],
-    maxx, maxy)
+pub fn j(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([0.0, 0.0, 1.0, 1.0],
+               [(1, 0), (1, 1), (1, 2), (0, 2)],
+               (1.0, 1.0),
+               maxx, maxy, boardy)
 }
 
-pub fn l(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([1.0, 0.5, 0.0, 1.0], [
-    vec![(0, 1), (1, 1), (2, 1), (2, 0)],
-    vec![(1, 0), (1, 1), (1, 2), (2, 2)],
-    vec![(0, 1), (1, 1), (2, 1), (0, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (0, 0)],],
-    maxx, maxy)
+pub fn l(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([1.0, 0.5, 0.0, 1.0],
+               [(0, 0), (0, 1), (0, 2), (1, 2)],
+               (0.0, 1.0),
+               maxx, maxy, boardy)
 }
 
-pub fn s(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([0.0, 1.0, 0.0, 1.0], [
-    vec![(0, 1), (1, 1), (1, 0), (2, 0)],
-    vec![(1, 0), (1, 1), (2, 1), (2, 2)],
-    vec![(0, 2), (1, 2), (1, 1), (2, 1)],
-    vec![(0, 0), (0, 1), (1, 1), (1, 2)],],
-    maxx, maxy)
+pub fn s(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([0.0, 1.0, 0.0, 1.0],
+               [(1, 0), (2, 0), (0, 1), (1, 1)],
+               (1.0, 1.0),
+               maxx, maxy, boardy)
 }
 
-pub fn z(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([1.0, 0.0, 0.0, 1.0], [
-    vec![(0, 0), (1, 0), (1, 1), (2, 1)],
-    vec![(1, 1), (1, 2), (2, 1), (2, 0)],
-    vec![(0, 1), (1, 1), (1, 2), (2, 2)],
-    vec![(1, 0), (1, 1), (0, 1), (0, 2)],],
-    maxx, maxy)
+pub fn z(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([1.0, 0.0, 0.0, 1.0],
+               [(1, 1), (2, 1), (0, 0), (1, 0)],
+               (1.0, 1.0),
+               maxx, maxy, boardy)
 }
 
-pub fn t(maxx: i8, maxy: i8) -> Piece {
-    Piece::new([0.5, 0.0, 0.8, 1.0], [
-    vec![(0, 1), (1, 1), (2, 1), (1, 0)],
-    vec![(1, 0), (1, 1), (1, 2), (2, 1)],
-    vec![(0, 1), (1, 1), (2, 1), (1, 2)],
-    vec![(1, 0), (1, 1), (1, 2), (0, 1)],],
-    maxx, maxy)
+pub fn t(maxx: i8, maxy: i8, boardy: i8) -> Piece {
+    Piece::new([0.5, 0.0, 0.8, 1.0],
+               [(1, 0), (0, 1), (1, 1), (2, 1)],
+               (1.0, 1.0),
+               maxx, maxy, boardy)
 }
+
